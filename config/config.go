26c8e3de1670fa9a503047a3856bdaff9deb450e
Vadim Vaskevich <vadskev@ya.ru>
2024-04-10 11:29:05 +0300

    fix: add file patch key

--- config/config.go
+++ config/config.go
@@ -8,20 +8,22 @@ import (
 const (
 	defaultServer      = "localhost:8080"
 	defaultBaseURL     = "http://localhost:8080"
-	defaultStoragePath = "./"
+	defaultStoragePath = "/tmp/short-url-db.json"
 )
 
 type Config struct {
-	Server   string
-	BaseURL  string
-	LogLevel string
+	Server          string
+	BaseURL         string
+	LogLevel        string
+	FileStoragePath string
 }
 
 func Load() *Config {
 	cfg := &Config{
-		Server:   defaultServer,
-		BaseURL:  defaultBaseURL,
-		LogLevel: "info",
+		Server:          defaultServer,
+		BaseURL:         defaultBaseURL,
+		LogLevel:        "info",
+		FileStoragePath: defaultStoragePath,
 	}
 
 	// get env
@@ -32,9 +34,14 @@ func Load() *Config {
 		cfg.BaseURL = envBaseURL
 	}
 
+	if envFileStoragePath := os.Getenv("FILE_STORAGE_PATH"); envFileStoragePath != "" {
+		cfg.FileStoragePath = envFileStoragePath
+	}
+
 	// get flag
 	flag.StringVar(&cfg.Server, "a", "localhost:8080", "server address; example: -a localhost:8080")
 	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "short url base; example: -b https://yandex.ru")
+	flag.StringVar(&cfg.FileStoragePath, "f", "/tmp/short-url-db.json", "file storage path; example: -f /tmp/short-url-db.json")
 	flag.Parse()
 
 	return cfg
