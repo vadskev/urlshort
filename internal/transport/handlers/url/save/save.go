4edf383ca0913dec45a7a8ab3416ee3161c9cb91
Vadim Vaskevich <vadskev@ya.ru>
2024-07-23 13:56:09 +0300

    fix: fix problem

--- internal/transport/handlers/url/save/save.go
+++ internal/transport/handlers/url/save/save.go
@@ -87,6 +87,12 @@ func New(log *zap.Logger, cfg *config.Config, store URLSaver) http.HandlerFunc {
 			return
 		}
 
+		_, err = w.Write([]byte(req.ResURL))
+		if err != nil {
+			log.Info("failed to write response", zp.Err(err))
+			return
+		}
+
 		// response OK
 		w.Header().Set("content-type", "text/plain")
 		w.WriteHeader(http.StatusCreated)
